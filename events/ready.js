e98360d979aae0636f56a2132e36f7b6be8b6698
IStayThatWayCoding <68560774+IStayThatWayCoding@users.noreply.github.com>
2021-12-08 14:12:06 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 module.exports = bot => {
     console.log(`${bot.user.username} is online`);
     
-    bot.user.setActivity("with santa! ❄", {
-      type: "STREAMING",
-      url: "https://www.twitch.tv/istaythatway"
+    bot.user.setActivity("the snow!", {
+      type: "WATCHING"
+//       url: "https://www.twitch.tv/istaythatway"
     });
 }
